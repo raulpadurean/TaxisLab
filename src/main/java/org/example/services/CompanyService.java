78950870f86d11f928a4dc7d4c5b677265c7f886
demkudominik <demkudominik@outlok.com>
2024-11-10 17:56:09 +0200

    add company service

---
+++ src/main/java/org/example/services/CompanyService.java
@@ -0,0 +1,34 @@
+package org.example.services;
+
+import org.example.models.Company;
+import org.example.repositories.IRepository;
+
+import java.util.List;
+
+public class CompanyService {
+    private final IRepository<Company> companyRepository;
+
+    public CompanyService(IRepository<Company> companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public void addCompany(Company company) {
+        companyRepository.create(company);
+    }
+
+    public Company getCompany(int id) {
+        return companyRepository.read(id);
+    }
+
+    public List<Company> getAllCompanies() {
+        return companyRepository.getAll();
+    }
+
+    public void updateCompany(Company company) {
+        companyRepository.update(company);
+    }
+
+    public void deleteCompany(int id) {
+        companyRepository.delete(id);
+    }
+}
