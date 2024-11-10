1090e846852486e86e290e5772c91e1cf1c671db
demkudominik <demkudominik@outlok.com>
2024-11-10 17:20:59 +0200

    add customservice model

---
+++ src/main/java/org/example/models/CustomService.java
@@ -0,0 +1,30 @@
+package org.example.models;
+
+public class CustomService extends BasicService {
+
+    private String extras;
+
+    public CustomService(int id, String name, double pricePerKm, String extras) {
+        super(id, name, pricePerKm); // Call the constructor of BasicService
+        this.extras = extras;
+    }
+
+    // Getters and setters for extras
+    public String getExtras() {
+        return extras;
+    }
+
+    public void setExtras(String extras) {
+        this.extras = extras;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomService{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", pricePerKm=" + getPricePerKm() +
+                ", extras='" + extras + '\'' +
+                '}';
+    }
+}
